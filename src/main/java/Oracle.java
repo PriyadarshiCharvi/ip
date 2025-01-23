a27b513881dfe72d36769c268af10968b8c5f5d7
Charvi Priyadarshi <>
2025-01-23 22:56:02 +0800

    Add todo, event and deadline tasks

--- src/main/java/Oracle.java
+++ src/main/java/Oracle.java
@@ -9,12 +9,8 @@ class Task {
         this.isDone = false;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]");
+        return (isDone ? "X" : " ");
     }
 
     public void markDone() {
@@ -24,7 +20,54 @@ class Task {
     public void markUndone() {
         this.isDone = false;
     }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    private final String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    private final String from;
+    private final String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
 
 public class Oracle {
     public static void main(String[] args) {
@@ -49,17 +92,49 @@ public class Oracle {
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("    " + (i + 1) + ". " + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
                     }
                     System.out.println("    ____________________________________________________________");
                 }
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[taskCount] = new Todo(description);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Got it. I've added this task to the list:");
+                System.out.println("    " + tasks[taskCount]);
+                System.out.println("    Now you have " + (taskCount + 1) + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+                taskCount++;
+            } else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split("/by");
+                String description = parts[0].trim();
+                String by = parts[1].trim();
+                tasks[taskCount] = new Deadline(description, by);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Got it. I've added this deadline to the list:");
+                System.out.println("    " + tasks[taskCount]);
+                System.out.println("    Now you have " + (taskCount + 1) + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+                taskCount++;
+            } else if (input.startsWith("event ")) {
+                String[] parts = input.substring(6).split("/from|/to");
+                String description = parts[0].trim();
+                String from = parts[1].trim();
+                String to = parts[2].trim();
+                tasks[taskCount] = new Event(description, from, to);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Got it. I've added this event to the list:");
+                System.out.println("    " + tasks[taskCount]);
+                System.out.println("    Now you have " + (taskCount + 1) + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+                taskCount++;
             } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 if (index >= 0 && index < taskCount) {
                     tasks[index].markDone();
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("    " + tasks[index].getStatusIcon() + " " + tasks[index].getDescription());
+                    System.out.println("    " + tasks[index]);
                     System.out.println("    ____________________________________________________________");
                 }
             } else if (input.startsWith("unmark")) {
@@ -68,7 +143,7 @@ public class Oracle {
                     tasks[index].markUndone();
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    OK, I've marked this task as not done yet:");
-                    System.out.println("      " + tasks[index].getStatusIcon() + " " + tasks[index].getDescription());
+                    System.out.println("    " + tasks[index]);
                     System.out.println("    ____________________________________________________________");
                 }
             } else if (input.equals("bye")) {
