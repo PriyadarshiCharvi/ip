eaabf778111942d2a94194eac3e0a1e6cd399a80
Charvi Priyadarshi <>
2025-01-23 22:19:49 +0800

    Add echo functionality

--- src/main/java/Oracle.java
+++ src/main/java/Oracle.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Oracle {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Oracle");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Oracle");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while(true) {
+            String input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + input);
+            System.out.println("    ____________________________________________________________");
+
+        }
+        in.close();
     }
 }
