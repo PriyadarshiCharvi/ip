09f2ca2f092339e6b56b28d7753ea75686d4f902
Charvi Priyadarshi <>
2025-01-23 22:27:25 +0800

    Implement task storage and list functionality

--- src/main/java/Oracle.java
+++ src/main/java/Oracle.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Oracle {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Oracle");
@@ -12,17 +14,30 @@ public class Oracle {
         while(true) {
             String input = in.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    There are no tasks in your list yet.");
+                    System.out.println("    ____________________________________________________________");
+                } else {
+                    System.out.println("    ____________________________________________________________");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (input.equals("bye")) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else {
+                tasks[taskCount] = input;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + input);
+                System.out.println("    ____________________________________________________________");
+                taskCount++;
             }
-
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + input);
-            System.out.println("    ____________________________________________________________");
-
         }
         in.close();
     }
